c670e098b0afdbccac471f93f4cffe2c4cbe0d9d
Eetu Mäkelä <eetu.makela@iki.fi>
2016-03-14 13:55:04 +0200

    update mark annotations

--- src/main/scala/MarkAnnotations.scala
+++ src/main/scala/MarkAnnotations.scala
@@ -24,31 +24,30 @@ import cc.mallet.pipe.CharSequenceLowercase
 import cc.mallet.pipe.TokenSequenceRemoveStopwords
 import java.io.File
 import cc.mallet.pipe.TokenSequenceRemoveStopPatterns
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 object MarkAnnotations {
   def main(args: Array[String]): Unit = {
-    val annotations = Source.fromFile("ner_testidata_places-nofuzzy4.txt").getLines.toSeq
-    val sannotations = annotations.filter(_.indexOf(' ') == -1).toSet
-    val mannotations = annotations.filter(_.indexOf(' ') != -1)
-    println(sannotations.size,'/',mannotations.size)
-    var text = Source.fromFile("ner_testidata_virke.txt").getLines()
-    val pw2 = new java.io.PrintWriter(new File("ner_testidata_words.bin"))
-    for (line <- text;word <- line.split(" ")) pw2.println(word)
-    pw2.close()
-    /*for (an <- mannotations) text = text.replaceAllLiterally(an, "<EnamexLocPpl>"+an+"\t</EnamexLocPpl>")
-    val pw = new java.io.PrintWriter(new File("ner_testidata_places-nofuzzy.bin"))
-    var inMulti = false
-    for (word <-text.split(" +")) {
-      if (word.startsWith("<EnamexLocPpl>")) {
-        pw.println(word.substring("<EnamexLocPpl>".length)+"\t<EnamexLocPpl>")
-        inMulti = true
-      } else if (word.contains("\t</EnamexLocPpl>")) {
-        pw.println(word.replace("\t</EnamexLocPpl>","")+"\t</EnamexLocPpl>")
-        inMulti = false
-      } else if (inMulti) pw.println(word+"\t<EnamexLocPpl>")
-      else if (sannotations.contains(word) || sannotations.contains(word.replaceAll("\\W*$",""))) pw.println(word+"\t<EnamexLocPpl/>")
-      else pw.println(word+"\tO")
+    val pw = new java.io.PrintWriter(new File("ner_testidata_places-tagged.bin"))
+    for (file <- new java.io.File("tmp/").listFiles.filter(_.getName.endsWith(".in")).sorted) {
+      val annotations = (parse(new java.io.File(file.getAbsolutePath.replace(".in",".out"))) \\ "matches" \\ classOf[JString])
+      val sannotations = annotations.filter(_.indexOf(' ') == -1).toSet
+      val mannotations = annotations.filter(_.indexOf(' ') != -1)
+      var text = Source.fromFile(file).getLines().mkString
+      for (an <- mannotations) text = text.replaceAllLiterally(an, "<EnamexLocPpl>"+an+"\t</EnamexLocPpl>")
+      var inMulti = false
+      for (word <-text.split(" +")) {
+        if (word.startsWith("<EnamexLocPpl>")) {
+          pw.println(word.replace("<EnamexLocPpl>","")+"\t<EnamexLocPpl>")
+          inMulti = true
+        } else if (word.contains("\t</EnamexLocPpl>")) {
+          pw.println(word.replace("\t</EnamexLocPpl>","")+"\t</EnamexLocPpl>")
+          inMulti = false
+        } else if (inMulti) pw.println(word+"\t<EnamexLocPpl>")
+        else if (sannotations.contains(word) || sannotations.contains(word.replaceAll("\\W*$",""))) pw.println(word+"\t<EnamexLocPpl/>")
+        else pw.println(word+"\tO")
+      }
     }
-    pw.close()*/
   }
 }
