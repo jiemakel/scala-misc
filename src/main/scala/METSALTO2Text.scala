32f35497bb9ee96ea202cfd73af5258cb7ffa3bc
Eetu Mäkelä <eetu.makela@iki.fi>
2016-04-07 18:13:02 +0200

    complete reworking of hierarchy handling

--- src/main/scala/METSALTO2Text.scala
+++ src/main/scala/METSALTO2Text.scala
@@ -40,14 +40,26 @@ object METSALTO2Text {
     f #:: (if (f.isDirectory) f.listFiles().toStream.flatMap(getFileTree) 
       else Stream.empty)
       
+  case class Image(hpos: Int, vpos: Int, width: Int, height: Int)
+  
+  def serializeImage(filename: String, image: Image) {
+    //FIXME: add image serialization
+  }
+  
+  case class Level(element: String, id: String, fileNamePrefix: String, content: StringBuilder, serialize: Boolean) {
+    val pages = new HashSet[Int]
+  }
+  
   def process(metsFile: File): Future[Unit] = Future {
       val directory = metsFile.getParentFile
       println("Processing: "+directory)
       new File(directory.getPath+"/extracted").mkdir()
       val prefix = directory.getPath+"/extracted/"+directory.getName+'_'
       val textBlocks = new HashMap[String,String]
-      val seenTextBlocks = new HashSet[String]
+      val imageBlocks = new HashMap[String,Image]
       val composedBlocks = new HashMap[String,HashSet[String]]
+      val seenTextBlocks = new HashSet[String]
+      val seenImageBlocks = new HashSet[String]
       val seenComposedBlocks = new HashSet[String]
       var break = false
       for (file <- new File(directory.getPath+"/alto").listFiles) {
@@ -58,6 +70,10 @@ object METSALTO2Text {
           case EvElemStart(_,"ComposedBlock",attrs,_) => 
             composedBlock = Some(new HashSet[String])
             composedBlocks.put(attrs("ID")(0).text,composedBlock.get)
+          case EvElemStart(_,"GraphicalElement",attrs,_) =>
+            val imageBlock = attrs("ID")(0).text
+            composedBlock.foreach(_+=imageBlock)
+            imageBlocks.put(imageBlock,Image(Integer.parseInt(attrs("HPOS")(0).text),Integer.parseInt(attrs("VPOS")(0).text),Integer.parseInt(attrs("WIDTH")(0).text),Integer.parseInt(attrs("HEIGHT")(0).text)))
           case EvElemEnd(_,"ComposedBlock") =>
             composedBlock = None 
           case EvElemStart(_,"TextBlock",attrs,_) => 
@@ -82,11 +98,9 @@ object METSALTO2Text {
       val s = Source.fromFile(metsFile,"UTF-8")
       val xml = new XMLEventReader(s)
       val articleMetadata: HashMap[String,String] = new HashMap
-      var advertisements = 0
-      var titleSections = 0
       while (xml.hasNext) xml.next match {
-        case EvElemStart(_,"dmdSec", attrs, _) if attrs("ID")(0).text.startsWith("MODSMD_ARTICLE") || attrs("ID")(0).text.startsWith("MODSMD_CHAP") || attrs("ID")(0).text.startsWith("MODSMD_LIST") =>
-          val article = attrs("ID")(0).text
+        case EvElemStart(_,"dmdSec", attrs, _) =>
+          val entity = attrs("ID")(0).text
           break = false
           while (xml.hasNext && !break) xml.next match {
             case EvElemStart(_,"mods",_,_) => break = true
@@ -109,68 +123,89 @@ object METSALTO2Text {
               indent = indent.substring(0,indent.length-2)
               metadata+=indent + "</"+label+">\n" 
           }
-          articleMetadata.put(article,metadata)
-        case EvElemStart(_,"div", attrs, _) if (attrs("TYPE")(0).text=="ARTICLE" || attrs("TYPE")(0).text=="TABLE_OF_CONTENTS" || attrs("TYPE")(0).text=="LIST" || attrs("TYPE")(0).text=="CONTRIBUTION" || attrs("TYPE")(0).text=="CHAPTER" || attrs("TYPE")(0).text=="ILLUSTRATION" || attrs("TYPE")(0).text=="ADVERTISEMENT" || attrs("TYPE")(0).text=="TABLE" || attrs("TYPE")(0).text=="TITLE_SECTION") => 
-          val atype = attrs("TYPE")(0).text.toLowerCase
-          val articleNumber = atype match {
-            case "advertisement" => 
-              advertisements += 1          
-              ""+advertisements
-            case "title_section" =>
-              titleSections += 1          
-              ""+titleSections
-            case "article" => attrs("DMDID")(0).text.substring(14)
-            case "chapter" | "list" | "table" | "illustration" | "contribution" | "table_of_contents" => attrs("DMDID")(0).text.substring(11)
-          }
-          val pw = new PrintWriter(new File(prefix+atype+'_'+articleNumber+".txt"))
-          var depth = 1
-          val pages = new HashSet[Int]
-          while (xml.hasNext && depth!=0) xml.next match {
-            case EvElemStart(_,"div",attrs,_) => 
-              depth += 1
-              attrs("TYPE")(0).text match {
-                case "TITLE" => pw.append("# ")
-                case "AUTHOR" => pw.append("## ")
-                case "OVERLINE" => 
-                  val breakDepth = depth - 1
-                  while (xml.hasNext && depth!=breakDepth) xml.next match {
-                    case EvElemEnd(_,"div") => depth -= 1
-                    case _ => 
-                  }
+          articleMetadata.put(entity,metadata)
+        case EvElemStart(_,"structMap", attrs, _) if (attrs("TYPE")(0).text=="LOGICAL") => 
+          var advertisements = 0
+          var titleSections =  0
+          var current = Level("","","",new StringBuilder(),false)
+          var hierarchy = Seq(current)
+          while (xml.hasNext) xml.next match {
+            case EvElemStart(_,"div", attrs, _) =>
+              val divType =  attrs("TYPE")(0).text.toLowerCase
+              divType match {
+                case "title" | "headline" | "title_of_work" => 
+                  current = Level(divType,"",current.fileNamePrefix,current.content,false)
+                  current.content.append("# ")
+                case "heading_text" => 
+                  current = Level(divType,"",current.fileNamePrefix,current.content,false)
+                  current.content.append("## ")
+                case "author" => 
+                  current = Level(divType,"",current.fileNamePrefix,current.content,false)
+                  current.content.append("### ")
+                case "item" => 
+                  current = Level(divType,"",current.fileNamePrefix,current.content,false)
+                  current.content.append(" * ")
+                case "metae_serial" | "paragraph" | "image" | "main" | "caption" | "textblock" | "item_caption" | "heading" | "overline" | "footnote" | "content" | "body_content" | "text" | "newspaper" | "volume" | "issue" | "body" =>
+                  current = Level(divType,"",current.fileNamePrefix,current.content,false)
                 case _ => 
+                  val articleNumber = divType match {
+                    case "advertisement" => 
+                      advertisements += 1          
+                      "_"+advertisements
+                    case "title_section" =>
+                      titleSections += 1          
+                      "_"+titleSections
+                    case "front" => ""
+                    case "back" => ""
+                    case _ if attrs("DMDID")!=null => "_"+attrs("DMDID")(0).text.replaceFirst("^[^\\d]*","")
+                    case _ => ""
+                  }
+                  current = Level(divType,if (attrs("DMDID")!=null) attrs("DMDID")(0).text else "",(if (current.fileNamePrefix!="") current.fileNamePrefix+"_" else "")+divType+articleNumber, new StringBuilder(),true)
               }
-            case EvElemStart(_,"area",attrs,_) => {
+              hierarchy = hierarchy :+ current 
+            case EvElemStart(_,"area",attrs,_) =>
               val areaId = attrs("BEGIN")(0).text
-              pages.add(Integer.parseInt(areaId.substring(1,areaId.indexOf('_'))))
+              current.pages.add(Integer.parseInt(areaId.substring(1,areaId.indexOf('_'))))
               if (textBlocks.contains(areaId)) {
-                pw.println(textBlocks(areaId))
+                current.content.append(textBlocks(areaId))
                 seenTextBlocks.add(areaId)
+              } else if (imageBlocks.contains(areaId)) {
+                serializeImage(prefix+current.fileNamePrefix+"_image_"+areaId,imageBlocks(areaId))
+                seenImageBlocks.add(areaId)
               } else {
                 for (block <- composedBlocks(areaId)) {
-                  pages.add(Integer.parseInt(block.substring(1,block.indexOf('_'))))
-                  pw.println(textBlocks(block))
-                  seenTextBlocks.add(block)
+                  current.pages.add(Integer.parseInt(block.substring(1,block.indexOf('_'))))
+                  if (imageBlocks.contains(block)) {
+                    serializeImage(prefix+current.fileNamePrefix+"_image_"+block,imageBlocks(block))
+                    seenImageBlocks.add(block)
+                  } else {
+                    current.content.append(textBlocks(block))
+                    seenTextBlocks.add(block)
+                  }
                 }
                 seenComposedBlocks.add(areaId)
               }
-            }
-            case EvElemEnd(_,"div") => depth -= 1
-            case _ => 
-          }
-          pw.close()
-          if (atype == "article" && articleMetadata.contains(attrs("DMDID")(0).text)) {
-            val pw = new PrintWriter(new File(prefix+atype+"_"+articleNumber+"_metadata.xml"))
-            pw.append("<metadata>\n")
-            pw.append(articleMetadata(attrs("DMDID")(0).text))
-            pw.append("<pages>"+pages.toSeq.sorted.mkString(",")+"</pages>\n")
-            pw.append("</metadata>\n")
-            pw.close()
-          } else {
-            val pw = new PrintWriter(new File(prefix+atype+"_"+articleNumber+"_metadata.xml"))
-            pw.append("<metadata>\n")
-            pw.append("<pages>"+pages.toSeq.sorted.mkString(",")+"</pages>\n")
-            pw.append("</metadata>\n")
-            pw.close()
+            case EvElemEnd(_,"div") =>
+              val hc = hierarchy.last
+              hierarchy = hierarchy.dropRight(1)
+              if (hc.element=="paragraph" || hc.element=="textblock") hc.content.append("\n")
+              if (hc.serialize) {
+                if (!hc.content.isEmpty) {
+                  val pw = new PrintWriter(new File(prefix+hc.fileNamePrefix+".txt"))
+                  pw.append(hc.content)
+                  pw.close()
+                }
+                val pw = new PrintWriter(new File(prefix+hc.fileNamePrefix+"_metadata.xml"))
+                pw.append("<metadata>\n")
+                if (articleMetadata.contains(hc.id))
+                  pw.append(articleMetadata(hc.id))
+                pw.append("<pages>"+hc.pages.toSeq.sorted.mkString(",")+"</pages>\n")
+                pw.append("</metadata>\n")
+                pw.close()
+              }
+              current=hierarchy.last
+              current.pages++=hc.pages
+            case _ =>  
           }
         case _ =>  
       }
@@ -208,7 +243,7 @@ object METSALTO2Text {
   def main(args: Array[String]): Unit = {
     val f = Future.sequence(for (dir<-args.toSeq;metsFile <- getFileTree(new File(dir)); if (metsFile.getName().endsWith("_mets.xml"))) yield {
       val f = process(metsFile)
-      f.onFailure { case t => println("An error has occured processing "+metsFile.getParentFile+": " + t.getMessage) }
+      f.onFailure { case t => println("An error has occured processing "+metsFile.getParentFile+": " + t.printStackTrace()) }
       f.onSuccess { case _ => println("Processed: "+metsFile.getParentFile) }
       f
     })
