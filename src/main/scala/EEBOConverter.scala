4ac14ea28af078409eb48ea30d0e2d486e6834ca
Eetu Mäkelä <eetu.makela@iki.fi>
2019-02-11 10:31:33 +0200

    add options and fix bugs in TCP converter

--- src/main/scala/EEBOConverter.scala
+++ src/main/scala/EEBOConverter.scala
@@ -1,6 +1,7 @@
 import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
 
 import ECCOXML2Text.attrsToString
+import com.bizo.mighty.csv.CSVWriter
 import org.rogach.scallop.ScallopConf
 
 import scala.collection.immutable.HashSet
@@ -62,6 +63,8 @@ object EEBOConverter extends ParallelProcessor {
     val omitStructure: Boolean,
     val onlyCoreTranscription: Boolean,
     val extractsAtDocumentLevel: Boolean,
+    val extractsAtPageLevel: Boolean,
+    val rehyphenate: Boolean,
     val startOffset: Int,
     val currentElem: String
    ) {
@@ -77,19 +80,22 @@ object EEBOConverter extends ParallelProcessor {
       this.omitStructure,
       this.onlyCoreTranscription,
       this.extractsAtDocumentLevel,
+      this.extractsAtPageLevel,
+      this.rehyphenate,
       startOffset,
       elem)
   }
 
   private class PageTracker(val pbw: PrintWriter, val dcontent: StringBuilder) {
-    var lastPage: String = "-1"
+    var currentPage: String = "0"
+    var subPage  = 1
+    val pcontent = new StringBuilder()
   }
 
   private def process(opts: ProcessOpts)(implicit xml: XMLEventReader, pt: PageTracker): String = {
-    val pcontent = new StringBuilder()
     val scontent = new StringBuilder()
     val content = new StringBuilder()
-    implicit val contents = Array(pcontent, scontent, content)
+    implicit val contents = Array(pt.pcontent, scontent, content)
     val divLevels = new ArrayBuffer[Int]()
     var currentDivLevel = 0
     divLevels += currentDivLevel
@@ -98,7 +104,6 @@ object EEBOConverter extends ParallelProcessor {
     var inTable = false
     var lastAddedNewLine = false
     var dpart = 1
-    var curPage = 0
     while (xml.hasNext && !break) {
       var addedNewLine = false
       xml.next match {
@@ -142,28 +147,37 @@ object EEBOConverter extends ParallelProcessor {
           val sw = new PrintWriter(if (opts.extractsAtDocumentLevel) cpo.outputRootPrefix+"_image_at_" + (opts.startOffset+content.length) + ".txt" else cpo.outputFilePrefix + ".txt")
           sw.append(imagedesc)
           sw.close()
+          if (opts.extractsAtPageLevel) {
+            val pcc = clean(pt.pcontent)
+            val sw = new PrintWriter(cpo.outputRootPrefix+"_image_at_page"+pt.currentPage+"_at_" + pcc.length + ".txt")
+            sw.append(imagedesc)
+            sw.close()
+          }
         }
         case EvElemEnd(_, "FIGDESC") => if (!opts.onlyCoreTranscription) append(" 〉")
         case EvElemStart(_, "HI", _, _) => if (!opts.omitEmphases) append("*")
         case EvElemEnd(_, "HI") => if (!opts.omitEmphases) append("*")
         case EvElemStart(_, "PB", attrs, _) => //MS="y" REF="48" N="90"
           val pid: String = attrs("REF").headOption.map(_.text).getOrElse("-1")
-          if (pid!=pt.lastPage) {
+          val pcc = clean(pt.pcontent)
+          if (pcc.nonEmpty || pt.currentPage != "0") {
+            val pw = new PrintWriter(new BufferedWriter(new FileWriter(new File(opts.outputRootPrefix + "_page" + pt.currentPage + (if (pid == pt.currentPage) {
+              pt.subPage += 1
+              "_subpage" + pt.subPage
+            } else "") + ".txt"), true)))
+            pw.append(pcc)
+            pw.close()
+            pt.pcontent.clear()
+          }
+          addedNewLine = true
+          if (pid!=pt.currentPage) {
             val bb = clean(pt.dcontent.toString + content.toString)
             val offset = opts.startOffset + bb.length
             pt.pbw.append("" + offset + ',' + pid + '\n')
-            pt.lastPage = pid
+            pt.subPage = 1
           }
+          pt.currentPage = pid
         case EvElemEnd(_, "PB") =>
-          val pcc = clean(pcontent)
-          if (pcc.nonEmpty) {
-            val pw = new PrintWriter(new BufferedWriter(new FileWriter(new File(opts.outputRootPrefix + "_page"+curPage+".txt"),true)))
-            pw.append(pcc)
-            pw.close()
-          }
-          pcontent.clear()
-          curPage += 1
-          addedNewLine = true
         case EvElemStart(_, elem, _, _) if elem == "NOTE" || elem == "HEADNOTE" || elem == "TAILNOTE" || elem == "DEL" || elem == "CORR" =>
           if (!opts.inlineNotes) {
             val cpo = opts.child(elem.toLowerCase+"_at_"+ content.length, opts.startOffset + content.length , elem)
@@ -171,9 +185,18 @@ object EEBOConverter extends ParallelProcessor {
             val sw = new PrintWriter(if (opts.extractsAtDocumentLevel) cpo.outputRootPrefix+"_"+elem.toLowerCase+"_at_" + (opts.startOffset+content.length) + ".txt" else cpo.outputFilePrefix + ".txt")
             sw.append(note)
             sw.close()
+            if (opts.extractsAtPageLevel) {
+              val pcc = clean(pt.pcontent)
+              val sw = new PrintWriter(cpo.outputRootPrefix+"_"+elem.toLowerCase+"_at_page"+pt.currentPage+"_at_" + pcc.length + ".txt")
+              sw.append(note)
+              sw.close()
+            }
           }
         case EvElemEnd(_, elem) if opts.inlineNotes && (elem == "NOTE" || elem == "HEADNOTE" || elem == "TAILNOTE" || elem == "DEL" || elem == "CORR") =>
-        case EvElemStart(_, "GAP", attrs, _) => if (!opts.onlyCoreTranscription) append(attrs.get("DISP").flatMap(_.headOption).map(_.text).getOrElse("〈?〉"))
+        case EvElemStart(_, "GAP", attrs, _) =>
+          val disp = attrs.get("DISP").flatMap(_.headOption).map(_.text).getOrElse("〈?〉")
+          gfw.synchronized(gfw.write(Seq(opts.outputRootPrefix,pt.currentPage,""+pt.subPage,disp)))
+          if (!opts.onlyCoreTranscription) append(disp)
         case EvElemEnd(_, "GAP") =>
         case EvElemStart(_, "LIST",_,_) => listDepth += 1
         case EvElemEnd(_, "LIST") =>
@@ -209,7 +232,10 @@ object EEBOConverter extends ParallelProcessor {
           } else if (content.isEmpty || content.last != '\n') append("\n")
         case EvText(text) =>
           val text2 = if (inTable) text.replaceAllLiterally("\n","") else if (lastAddedNewLine && text.head=='\n') text.tail else text
-          append(text2.replaceAllLiterally("|","").replaceAllLiterally("∣",""))
+          if (opts.rehyphenate)
+            append(text2.replaceAllLiterally("|","-\n").replaceAllLiterally("∣","-\n"))
+          else
+            append(text2.replaceAllLiterally("|","").replaceAllLiterally("∣",""))
         case er: EvEntityRef => append(decodeEntity(er.entity))
         case EvComment(comment) if comment == " unknown entity apos; " => append("'")
         case EvComment(comment) if comment.startsWith(" unknown entity") =>
@@ -225,8 +251,8 @@ object EEBOConverter extends ParallelProcessor {
   }
 
 
-  def index(file: File, dest: String, prefixLength: Int, inlineNotes: Boolean, omitEmphases: Boolean, omitStructure: Boolean, onlyCoreTranscription: Boolean, extractsAtDocumentLevel: Boolean): Unit = {
-    val opts = new ProcessOpts(file.getAbsolutePath.replace(".xml",""),file.getAbsolutePath.replace(".xml",""), dest, prefixLength, inlineNotes, omitEmphases, omitStructure, onlyCoreTranscription, extractsAtDocumentLevel, 0, null)
+  def index(file: File, dest: String, prefixLength: Int, inlineNotes: Boolean, omitEmphases: Boolean, omitStructure: Boolean, onlyCoreTranscription: Boolean, extractsAtDocumentLevel: Boolean, extractsAtPageLevel: Boolean, rehyphenate: Boolean): Unit = {
+    val opts = new ProcessOpts(file.getAbsolutePath.replace(".xml",""),file.getAbsolutePath.replace(".xml",""), dest, prefixLength, inlineNotes, omitEmphases, omitStructure, onlyCoreTranscription, extractsAtDocumentLevel, extractsAtPageLevel, rehyphenate, 0, null)
     implicit val xml = new XMLEventReader(Source.fromFile(file, "UTF-8"))
     val dcontent = new StringBuilder()
     new File(dest + file.getParentFile.getAbsolutePath.substring(prefixLength)).mkdirs()
@@ -280,12 +306,21 @@ object EEBOConverter extends ParallelProcessor {
       cw.append(clean(dcontent))
       cw.close()
     }
+    val pcc = clean(pt.pcontent)
+    if (pcc.nonEmpty || pt.currentPage != "0") {
+      val pw = new PrintWriter(new BufferedWriter(new FileWriter(new File(opts.outputRootPrefix + "_page" + pt.currentPage + (if (pt.subPage!=1) {
+        "_subpage" + pt.subPage
+      } else "") + ".txt"), true)))
+      pw.append(pcc)
+      pw.close()
+      pt.pcontent.clear()
+    }
     pbw.close()
     logger.info("Successfully processed "+file)
   }
 
+  var gfw: CSVWriter = _
 
-  
   def main(args: Array[String]): Unit = {
     val opts = new ScallopConf(args) {
       val directories = trailArg[List[String]](required = true)
@@ -295,6 +330,8 @@ object EEBOConverter extends ParallelProcessor {
       val omitStructure = opt[Boolean]()
       val omitEmphases = opt[Boolean]()
       val extractsAtDocumentLevel = opt[Boolean]()
+      val extractsAtPageLevel = opt[Boolean]()
+      val rehyphenate = opt[Boolean]()
       verify()
     }
     val inlineNotes = opts.inlineNotes()
@@ -302,10 +339,13 @@ object EEBOConverter extends ParallelProcessor {
     val omitStructure = opts.omitStructure() || onlyCoreTranscription
     val omitEmphases = opts.omitEmphases() || omitStructure
     val extractsAtDocumentLevel = opts.extractsAtDocumentLevel()
+    val extractsAtPageLevel = opts.extractsAtPageLevel()
+    val rehyphenate = opts.rehyphenate()
     val dest = opts.dest()
     new File(dest).mkdirs()
     val lr = "<!ENTITY (.*?) \"(.*?)\"".r
     val lr2 = "&#h(.*?);".r
+    gfw = CSVWriter(dest+"/gaps.csv")
     for (line <- Source.fromInputStream(getClass.getResourceAsStream("Eebochar-xml-all-view.ent")).getLines; m <- lr.findFirstMatchIn(line)) {
       val entity = m.group(1)
       val replacement = lr2.replaceAllIn(m.group(2), m => Integer.valueOf(m.group(1), 16).toChar.toString)
@@ -314,7 +354,8 @@ object EEBOConverter extends ParallelProcessor {
     feedAndProcessFedTasksInParallel(() => {
       for (dir <- opts.directories(); f = new File(dir); prefixLength = f.getAbsolutePath.length)
         getFileTree(f).filter(_.getName.endsWith(".xml")).foreach(file => addTask(file.getPath, () => index(file,
-          dest, prefixLength, inlineNotes, omitEmphases, omitStructure, onlyCoreTranscription, extractsAtDocumentLevel)))
+          dest, prefixLength, inlineNotes, omitEmphases, omitStructure, onlyCoreTranscription, extractsAtDocumentLevel, extractsAtPageLevel, rehyphenate)))
     })
+    gfw.close()
   }
 }
